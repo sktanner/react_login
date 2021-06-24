e38f1b2187809012256d512c892c851ebf0281cf
Sarah Tanner <sktanner92@gmail.com>
2021-06-24 19:37:46 -0400

    Login challenge

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      
+      <form>
+        <h1>Login</h1>
+        <label>Username: </label>
+        <input></input>
+        <br />
+        <label>Password: </label>
+        <input></input>
+        <a>Don't have an account?</a>
+        <button>Login</button>
+      </form>
+
+
+    </div>
+  );
+}
+
+export default App;
